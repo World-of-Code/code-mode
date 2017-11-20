fe5bc92ac226f9c99a219b03b525197335171868
apdjz <14apdjz@gmail.com>
2017-11-20 11:41:04 -0500

    changed import--file location

--- client/components/Repl/example/index.jsx
+++ client/components/Repl/example/index.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import AceEditor from '../src/ace.js';
+import AceEditor from '../src/ace.jsx';
 import 'brace/mode/jsx';
 
 const languages = [
