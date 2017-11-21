80ccc2db668f24da64321cb88578ad2c5e2307d2
Veekas Shrivastava <veekas@veekasmeansprogress.com>
2017-11-21 16:10:51 -0500

    feat: inject into DOM in progress

--- client/index.jsx
+++ client/index.jsx
@@ -6,12 +6,32 @@ import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import Routes from './routes'
+import DrawerWrapper from './components/Drawer'
 import './socket'
+import '../public/style/main.css'
+import registerServiceWorker from './registerServiceWorker'
 
+// Get the element to prepend our app to. This could be any element on a specific website or even just `document.body`.
+const viewport = document.getElementById('viewport');
+
+if (viewport) viewport.prepend(document.getElementById('app'));
 
 ReactDOM.render(
   <Provider store={ store }>
-    <Routes />
+  <DrawerWrapper />
   </Provider>,
   document.getElementById('app')
 )
+
+registerServiceWorker();
+
+// if (viewport) viewport.prepend(document.getElementById('app'));
+
+
+// Create a div to render the <App /> component to.
+// const app = document.createElement('div');
+
+// Set the app element's id to `root`. This is the same as the element that create-react-app renders to by default so it will work on the local server too.
+// app.id = 'root';
+
+// Prepend the <App /> component to the viewport element if it exists. You could also use `appendChild` depending on your needs.
