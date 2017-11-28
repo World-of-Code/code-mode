8eb1cec1444d6403f74181de25fce4096dee48a0
Veekas Shrivastava <veekas@users.noreply.github.com>
2017-11-28 17:37:45 -0500

    chore: remove console.log

--- chrome/js/toggle.js
+++ chrome/js/toggle.js
@@ -3,7 +3,6 @@ function toggle()
 
     var toggle = document.getElementById("toggle");
     return function() {
-        console.log(chrome.management.setEnabled)
         if(chrome.management.setEnabled === 0)
             chrome.management.setEnabled = 1
         else
