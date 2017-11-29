1b912e64bbe6be502dc7ceb3e200bbdf662c73c3
Kyle Uehlein <kyleuehlein@gmail.com>
2017-11-28 23:35:09 -0500

    fix: fix import

--- client/store/user/user.spec.js
+++ client/store/user/user.spec.js
@@ -7,7 +7,7 @@ import MockAdapter from 'axios-mock-adapter'
 import configureMockStore from 'redux-mock-store'
 import thunkMiddleware from 'redux-thunk'
 import history from '../../history'
-import { BACK_END } from '../store'
+import { BACK_END } from '../../store'
 
 
 const middlewares = [thunkMiddleware]
