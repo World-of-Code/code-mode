c8da1115ff8422cfa97a52b021aa33a5e59886cc
Kyle Uehlein <kyleuehlein@gmail.com>
2017-11-28 23:28:20 -0500

    fix: fix logout bug

--- client/store/user/user.spec.js
+++ client/store/user/user.spec.js
@@ -42,7 +42,7 @@ describe('thunk creators', () => {
 
   describe('logout', () => {
     it('logout: eventually dispatches the REMOVE_USER action', () => {
-      mockAxios.onPost('https://code-mode.herokuapp.com/auth/logout').replyOnce(204)
+      mockAxios.onPost('http://localhost:8080/auth/logout').replyOnce(204)
       return store.dispatch(logout())
         .then(() => {
           const actions = store.getActions()
